5589a5ae14dc13c3907545a775b54f9355ea2d34
Enrico Elizan <eelizanjr@outlook.com>
2025-04-29 18:29:27 -0400

    Added vector store and emeddings

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import streamlit as st 
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter # from langchain 
+from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+# from langchain.vectorstores import FAISS
+from langchain.vectorstores import Chroma
+
 
 # take pdf documents and return a single string of text containing all the content 
 def get_pdf_text(pdf_docs):
@@ -10,6 +15,25 @@ def get_pdf_text(pdf_docs):
         for page in pdf_reader.pages: 
             text += page.extract_text()
     return text 
+
+# split text into chucks to be fed into model 
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000, # 1000 characters 
+        chunk_overlap=200,
+        length_function=len # len from python 
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+# use chunks to create vector store
+def get_vectorstore(text_chunks):
+    embeddings = OpenAIEmbeddings()  # or HuggingFaceInstructEmbeddings
+    vectorstore = Chroma.from_texts(texts=text_chunks, embedding=embeddings)
+    print("✅ Vectorstore created:", vectorstore)
+    return vectorstore
+
 
 def main(): 
     load_dotenv()
@@ -26,11 +50,12 @@ def main():
             with st.spinner("Processing"):
                 # get pdf text 
                 raw_text = get_pdf_text(pdf_docs)
-                st.write(raw_text)
 
                 # get the text chunks 
+                text_chunks = get_text_chunks(raw_text)
 
                 # create vector store with embeddings 
+                vectorstore = get_vectorstore(text_chunks)
 
 if __name__ == '__main__':
     main()
